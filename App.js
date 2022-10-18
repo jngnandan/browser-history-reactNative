e775220e4dfa3e3d665a02f52a5de1fbfcb7f9c1
jngnandan <jngnandan@gmail.com>
2022-10-18 11:23:40 +0100

    History items are added

--- App.js
+++ App.js
@@ -1,20 +1,122 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, Image, TextInput, FlatList } from 'react-native';
+// import Constants from 'expo-constants';
+
+// You can import from local files
+// import AssetExample from './components/AssetExample';
+import HistoryItems from './components/HistoryItems'
+
+// or any pure javascript modules available in npm
+// import { Card } from 'react-native-paper';
+// These are the list used in the application. You can move them to any component needed.
+
+import {useState} from 'react'
+
+const initialHistoryList = [
+  {
+    id: 0,
+    timeAccessed: '07:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/instagram-img.png',
+    title: 'Instagram',
+    domainUrl: 'instagram.com',
+  },
+  {
+    id: 1,
+    timeAccessed: '05:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/twitter-img.png',
+    title: 'Twitter. It’s what’s happening / Twitter',
+    domainUrl: 'twitter.com',
+  },
+  {
+    id: 2,
+    timeAccessed: '04:35 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/facebook-img.png',
+    title: 'Facebook – log in or sign up',
+    domainUrl: 'facebook.com',
+  },
+  {
+    id: 3,
+    timeAccessed: '04:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/linkedin-img.png',
+    title: 'LinkedIn: Log In or Sign Up',
+    domainUrl: 'linkedin.com',
+  },
+  {
+    id: 4,
+    timeAccessed: '04:00 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/hashnode-img.png',
+    title: 'Hashnode: Everything you need to start blogging as a developer!',
+    domainUrl: 'hashnode.com',
+  },
+  {
+    id: 5,
+    timeAccessed: '03:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/github-img.png',
+    title: 'GitHub: Where the world builds software · GitHub',
+    domainUrl: 'github.com',
+  },
+
+  {
+    id: 6,
+    timeAccessed: '02:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/react-img.png',
+    title: 'React – A JavaScript library for building user interfaces',
+    domainUrl: 'reactjs.org',
+  },
+  {
+    id: 7,
+    timeAccessed: '01:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/stackoverflow-img.png',
+    title: 'Stack Overflow - Where Developers Learn, Share, & Build Careers',
+    domainUrl: 'stackoverflow.com',
+  },
+
+  {
+    id: 8,
+    timeAccessed: '09:25 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/gmail-img.png',
+    title: 'Gmail',
+    domainUrl: 'mail.google.com',
+  },
+  {
+    id: 9,
+    timeAccessed: '09:00 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/google-img.png',
+    title: 'Google',
+    domainUrl: 'google.com',
+  },
+]
+
 
 export default function App() {
+
+  const [search, setSearch] = useState(initialHistoryList)
+  const [input, setInput] = useState('')
+
+  const updateSuggestions = ({text}) => {
+     return setSearch(initialHistoryList.filter((item) => item.title.toLowerCase().includes(text)))
+    // setSearch(keyword)
+  }
+
+  // const updateSearch = ({text}) => {
+  //   setInput(text)
+  // }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{paddingTop: 50}}>
+      <Text>{input}</Text>
+      <View style={{backgroundColor: 'blue', flexDirection: 'row', alignItems: 'center', paddingVertical: 8, justifyContent: 'space-around'}}>
+      <Image source={{uri: 'https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png'}} style={{height: 15, width: 60}} />
+      <TextInput onChangeText={updateSuggestions} style={{borderWidth: 1, width: 200, height: 40, borderRadius: 6,}} />
+      </View>
+      <View>
+            <FlatList
+             data={search}
+             renderItem={({item}) => (
+               <HistoryItems browse={item} />
+          )} />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/HistoryItems.js
@@ -0,0 +1,22 @@
+
+import {View, Text, Image} from 'react-native'
+
+const HistoryItems = ({browse}) => {
+
+  return (
+    <View style={{flexDirection: 'row', justifyContent: 'start', alignItems: 'center', paddingVertical: 25, flexGrow: 1}}>
+      <Text style={{fontSize: 12, paddingLeft: 14}}>{browse.timeAccessed}</Text>
+      <Image source={{uri: browse.logoUrl}} style={{height:19, width: 19, marginLeft: 20}}/>
+      <Text style={{paddingLeft: 20, width: 270,}}>{browse.title}</Text>
+      <Image source={{uri: 'https://assets.ccbp.in/frontend/react-js/delete-img.png'}} style={{height:19,   width: 19, marginHorizontal: 10}}/>
+    </View>
+  )
+}
+
+export default HistoryItems
+
+    // id: 9,
+    // timeAccessed: '09:00 AM',
+    // logoUrl: 'https://assets.ccbp.in/frontend/react-js/google-img.png',
+    // title: 'Google',
+    // domainUrl: 'google.com',
